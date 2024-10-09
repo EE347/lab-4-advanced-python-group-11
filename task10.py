cc91443c961b3bb6f9391f8e8ab83c8b25f75722
emily120403 <e.mullarkey5@universityofgalway.ie>
2024-10-09 16:32:08 +0100

    Task 9

--- task10.py
+++ task10.py
@@ -1,3 +1,20 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+
+
+
+
+
+plt.plot(dates, META, color = 'blue', label = 'META')
+plt.plot(dates, AAPL, color = 'black', label = 'AAPL')
+plt.plot(dates, AMZN, color = 'green', label = 'AMZN')
+plt.plot(dates, NFLX, color = 'red', label = 'NFLX')
+plt.plot(dates, NVDA, color = 'magenta', label = 'NVDA')
+plt.plot(dates, GOOGL, color = 'orange', label = 'GOOGL')
+
+plt.xlabel('Date')
+plt.ylabel('Price (USD)')
+plt.title('FAANNG Performance')
+plt.legend()
--- task9.py
+++ task9.py
@@ -1,3 +1,33 @@
 import csv
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+data = np.loadtxt('task9.csv', delimiter =',', dtype = str, skiprows = 1)
+
+dates = data[:,0]
+META = data[:,1].astype(float)
+AAPL = data[:,2].astype(float)
+AMZN = data[:,3].astype(float)
+NFLX = data[:,4].astype(float)
+NVDA = data[:,5].astype(float)
+GOOGL = data[:,6].astype(float)
+
+dates = [datetime.strptime(date, '%d/%m/%Y') for date in dates]
+
+plt.figure(figsize=(12,6))
+
+plt.plot(dates, META)
+plt.plot(dates, AAPL)
+plt.plot(dates, AMZN)
+plt.plot(dates, NFLX)
+plt.plot(dates, NVDA)
+plt.plot(dates, GOOGL)
+
+plt.xlabel('Date')
+plt.ylabel('Price (USD)')
+plt.title('FAANNG Performance')
+plt.legend()
+
+plt.show()
