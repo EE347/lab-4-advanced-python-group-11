71c32bfb2fcc2d324ac7a9e5adaa2e7d584af802
emily120403 <e.mullarkey5@universityofgalway.ie>
2024-10-09 15:51:23 +0100

    Task 5

--- task5.py
+++ task5.py
@@ -1,2 +1,18 @@
 import csv
 
+with open("task5.csv", "w", newline = "") as file:
+    writer = csv.writer(file)
+
+    while True:
+        name = input("Enter your name (quit to end): ")
+        if name.lower() == "quit":
+            break
+
+        writer.writerow([name])
+
+with open("task5.csv", "r") as file:
+    reader = csv.reader(file)
+
+    print("\nNames entered: ")
+    for row in reader:
+        print(row[0])
